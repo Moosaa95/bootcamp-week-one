4dc21567345f4a5d3edd84ccd985826da9c8876f
Musa Abdullahi <moosaabdullahi45@gmail.com>
2020-11-18 17:19:11 +0100

    create proxy function

---
+++ exercise/lab-three/proxy.js
@@ -0,0 +1,34 @@
+const handler = {
+    get: function(obj, prop) {
+        if (prop === 'id') {
+            //throw an error if id is being accessed
+            throw new Error('cannot access private key')
+        } else if (!(prop in obj)) {
+            return prop.split('_').map(function(part) {
+                return obj[part].toUpperCase()
+            }).join(' ')
+        } else {
+            return obj[prop];
+        }
+    },
+    set: function(obj, prop, value) {
+        if (value < obj['age']) {
+            throw new Error('You are too young to work with us')
+        } else {
+            obj[prop] = value
+        }
+    }
+
+}
+
+
+const Staff = {
+    id: 1,
+    first: 'Angela',
+    last: 'Jikwoyi',
+    age: 30
+}
+
+const proxyStaff = new Proxy(Staff, handler)
+
+console.log(proxyStaff.age = 44);
